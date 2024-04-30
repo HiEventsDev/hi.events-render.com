dcea0872afeffd0e4b927d2fa5bc3292d226d135
Dave Earley <dave.m.earley@gmail.com>
2024-05-01 00:06:40 +0100

    fix RENDER_URLS

--- render-start.sh
+++ render-start.sh
@@ -3,6 +3,6 @@
 export VITE_API_URL_CLIENT=${RENDER_EXTERNAL_URL:-"/api"}/api
 export VITE_API_URL_SERVER=http://localhost:80/api
 export VITE_FRONTEND_URL=${RENDER_EXTERNAL_URL:-"/"}
-export VITE_STRIPE_PUBLISHABLE_KEY=${STRIPE_PUBLISHABLE_KEY:-"change-me"}
+export VITE_STRIPE_PUBLISHABLE_KEY=${VITE_STRIPE_PUBLISHABLE_KEY:-"change-me"}
 
 exec /startup.sh
