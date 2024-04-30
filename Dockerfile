e29a885fcf940f0ef997f4294555b75755b0c73c
Dave Earley <dave.m.earley@gmail.com>
2024-04-30 23:23:23 +0100

    update render-start.sh

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM daveearley/hi.events-all-in-one
 
 COPY render-start.sh /render-start.sh
 RUN chmod +x /render-start.sh
-RUN /render-start.sh
+
+CMD ["/render-start.sh"]
+
--- render-start.sh
+++ render-start.sh
@@ -5,20 +5,4 @@ export VITE_API_URL_SERVER=http://localhost:80/api
 export VITE_FRONTEND_URL=$RENDER_EXTERNAL_URL
 export VITE_STRIPE_PUBLISHABLE_KEY=$STRIPE_PUBLISHABLE_KEY
 
-cd /app/backend
-
-if ! php artisan migrate --force; then
-    echo "\033[0;31mERROR: Migrations could not complete. Check the error above.\033[0m"
-    echo "\033[0;31mERROR: Ensure DATABASE_URL is set.\033[0m"
-fi
-
-# Clear caches
-php artisan cache:clear
-php artisan config:clear
-php artisan route:clear
-php artisan view:clear
-
-chown -R www-data:www-data /app/backend
-chmod -R 777 /app/backend/storage /app/backend/bootstrap/cache
-
-exec /usr/bin/supervisord -c /etc/supervisord.conf
+exec /startup.sh
