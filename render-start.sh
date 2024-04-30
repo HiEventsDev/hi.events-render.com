30faf868ca16d25edb0f3805c360f65daa5f5351
Dave Earley <dave.m.earley@gmail.com>
2024-04-30 23:53:22 +0100

    fix RENDER_URLS

--- render-start.sh
+++ render-start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export VITE_API_URL_CLIENT=${VITE_API_URL_CLIENT:-"/api"}
-export VITE_API_URL_SERVER=${VITE_API_URL_SERVER:-"http://localhost:80/api"}
-export VITE_FRONTEND_URL=${VITE_FRONTEND_URL:-"/"}
-export VITE_STRIPE_PUBLISHABLE_KEY=${VITE_STRIPE_PUBLISHABLE_KEY:-"change-me"}
+export VITE_API_URL_CLIENT=${RENDER_EXTERNAL_URL:-"/api"}/api
+export VITE_API_URL_SERVER=http://localhost:80/api
+export VITE_FRONTEND_URL=${RENDER_EXTERNAL_URL:-"/"}
+export VITE_STRIPE_PUBLISHABLE_KEY=${STRIPE_PUBLISHABLE_KEY:-"change-me"}
 
 exec /startup.sh
