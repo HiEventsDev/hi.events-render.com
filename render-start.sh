c135c6a1dac387214e6ce7fc5326677c224507d9
Dave Earley <dave.m.earley@gmail.com>
2024-05-04 09:07:31 +0100

    Add storage envs

--- render-start.sh
+++ render-start.sh
@@ -2,13 +2,15 @@
 
 if [ -z "$RENDER_EXTERNAL_URL" ]; then
     export VITE_API_URL_CLIENT="/api"
+    export APP_CND_URL="/api/storage"
 else
     export VITE_API_URL_CLIENT="${RENDER_EXTERNAL_URL}/api"
+    export APP_CND_URL="${RENDER_EXTERNAL_URL}/api/storage"
 fi
 
 export VITE_API_URL_SERVER=http://localhost:80/api
 export VITE_FRONTEND_URL=${RENDER_EXTERNAL_URL:-"/"}
-export VITE_STRIPE_PUBLISHABLE_KEY=${VIRE_STRIPE_PUBLISHABLE_KEY:-"change-me"}
+export VITE_STRIPE_PUBLISHABLE_KEY=${VIRE_STRIPE_PUBLISHABLE_KEY:-"pk_test-change-me"}
 
 echo "Starting with the following environment variables:"
 echo "RENDER_EXTERNAL_URL=${RENDER_EXTERNAL_URL}"
@@ -16,5 +18,6 @@ echo "VITE_API_URL_CLIENT=${VITE_API_URL_CLIENT}"
 echo "VITE_API_URL_SERVER=${VITE_API_URL_SERVER}"
 echo "VITE_FRONTEND_URL=${VITE_FRONTEND_URL}"
 echo "VITE_STRIPE_PUBLISHABLE_KEY=${VITE_STRIPE_PUBLISHABLE_KEY}"
+echo "APP_CND_URL=${APP_CND_URL}"
 
 exec /startup.sh
